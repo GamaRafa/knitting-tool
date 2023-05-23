f866352cd2e5894101f462050ce0ed558ef55f44
Rafael Gama <rafa.gama.em7@gmail.com>
2023-05-23 19:09:07 -0300

    making the navbar

--- reactapp/src/App/App.jsx
+++ reactapp/src/App/App.jsx
@@ -1,6 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 
+import fabric from '../icons/fabric.png'
+import minus from '../icons/minus.png'
+import plus from '../icons/plus.png'
+import row_counter from '../icons/row-counter.png'
+import row_rep from '../icons/row-rep.png'
+
 export function App(){
 
     return(
@@ -8,10 +14,36 @@ export function App(){
             <p>Choose a tool from the navbar</p>
             <div className='navbar'>
                 <ul>
-                    <li><a href='#'>Row counter</a></li>
-                    <li><a href='#'>Row counter with repetitions</a></li>
-                    <li><a href='#'>Increase calculator</a></li>
-                    <li><a href='#'>Decrease calculator</a></li>
+                    <li>
+                        <a href='#' className='active'>
+                            <span className='icon'><img src={fabric} alt="" /></span>
+                            <span>Home</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='#'>
+                            <span className='icon'><img src={row_counter} alt="" /></span>
+                            <span>Row counter</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='#'>
+                            <span className='icon'><img src={row_rep} alt="" /></span>
+                            <span>Row counter with repetitions</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <span className='icon'><img src={plus} alt="" /></span>
+                            <span>Increase calculator</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <span className='icon'><img src={minus} alt="" /></span>
+                            <span>Decrease calculator</span>
+                        </a>
+                    </li>
                 </ul>
             </div>
         </div>
