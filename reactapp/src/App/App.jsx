5c04a8cd450c7815b5ba72fcf16b4f0f3f4600ab
Rafael Gama <rafa.gama.em7@gmail.com>
2023-05-24 11:43:50 -0300

    changed color of navbar, add descriptions

--- reactapp/src/App/App.jsx
+++ reactapp/src/App/App.jsx
@@ -11,7 +11,7 @@ export function App(){
 
     return(
         <div className='app'>
-            <p>Choose a tool from the navbar</p>
+            <h2>Choose a tool from the navbar</h2>
             <div className='navbar'>
                 <ul>
                     <li>
@@ -45,6 +45,24 @@ export function App(){
                         </a>
                     </li>
                 </ul>
+            </div>
+            <div className='description'>
+                <p>
+                    The <strong>Row counter</strong> tool keeps track of how many rows you've knitted.
+                </p>
+                <br />
+                <p>
+                    The <strong>Row counter with repetitions</strong> tool keeps track of how many rows you've knitted, and how many times you've repeated the pattern.
+                </p>
+                <br />
+                <p>
+                    The <strong>Increase calculator</strong> tool calculates the distribution of increases for a given number of stitches.
+                </p>
+                <br />
+                <p>
+                    The <strong>Decrease calculator</strong> tool calculates the distribution of decreases for a given number of stitches.
+                </p>
+                <br />
             </div>
         </div>
     )
